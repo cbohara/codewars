edd17911434c70f7017b235a060f0ebcf6742b5c
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-03-07 07:34:50 -0800

    completed kata

--- odd_one_out.py
+++ odd_one_out.py
@@ -1,14 +1,35 @@
 import sys
 
 
+def is_odd(num):
+    """Determine if input number is odd."""
+    return True if num % 2 != 0 else False
+
+
+def is_even(num):
+    """Determine if input number is even."""
+    return True if num % 2 == 0 else False
+
+
 def odd_one_out(numbers):
     """Determine position of number that is the only odd or only even number in list."""
+    odds = list(filter(is_odd, numbers))
+    evens = list(filter(is_even, numbers))
+
+    if len(evens) == 1:
+        for index, num in enumerate(numbers):
+            if is_even(num):
+                return index + 1
+    else:
+        for index, num in enumerate(numbers):
+            if is_odd(num):
+                return index + 1
     
 
-
 def main(sys_argv):
+    """Transform string of numbers into list of ints and input to function."""
     numbers = [int(x) for x in sys.argv[1].split(' ')]
-    odd_one_out(numbers)
+    print(odd_one_out(numbers))
 
 
 if __name__ == "__main__":
